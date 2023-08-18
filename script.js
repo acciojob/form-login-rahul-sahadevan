2a19dc1b157711e0d6d22854c8d0e656dcb021d7
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-18 09:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
+	const getForm = document.getElementById("form1");
+    getfname = getForm[0].value;
+    getsname = getForm[1].value;
+    alert(getfname+" "+getsname);
     //Write your code here
 
 }
